b770f43a477330382174ef687a2311251540e256
Guzanov Boris <borisgodunofff@yandex.ru>
2020-12-10 00:20:38 +0300

    Add

--- include/Polinom.h
+++ include/Polinom.h
@@ -1,5 +1,72 @@
 ﻿#include "CircleList.h"
-#include <cstdlib>
+#include "Monom.h"
 #include "string"
 using namespace std;
+class Polinom
+{
+private:
+    CircleList sp;
+    int Maxst;
+public:
+    Polinom(string str = "", int Maxst_ = 10)
+    {
 
+        Maxst = Maxst_;
+        if (str != "")
+        {
+            int k;
+            string* w;
+            w = new string[str.length() + 2];
+            StringToWord(str, w, k);
+            for (int i = 0; i < k; i++)
+            {
+                string s = w[i];
+                sp.AddMonom(s);
+            }
+        }
+    }
+    ~Polinom() {};
+    Polinom(const Polinom& tmp)
+    {
+        sp = tmp.sp;
+        Maxst = tmp.Maxst;
+    }
+    Polinom& operator=(Polinom tmp)
+    {
+        sp = tmp.sp;
+        Maxst = tmp.Maxst;
+        return *this;
+    }
+    Polinom operator+(Polinom tmp)
+    {
+        Polinom res;
+        if (Maxst == tmp.Maxst)
+        {
+            res.sp = sp + tmp.sp;
+        }
+        return res;
+    }
+    Polinom operator-(Polinom tmp)
+    {
+        Polinom res;
+        if (Maxst == tmp.Maxst)
+        {
+            res.sp = sp - tmp.sp;
+        }
+        return res;
+    }
+    Polinom operator*(Polinom tmp)
+    {
+        Polinom res;
+        if (Maxst == tmp.Maxst)
+        {
+            res.sp = sp.multiply(tmp.sp, Maxst);
+        }
+        return res;
+    }
+    string Getpolinom()
+    {
+        return sp.Getpolinom(Maxst);
+    }
+    void StringToWord(string str, string* w, int& k);
+};
--- src/CircleList.cpp
+++ src/CircleList.cpp
@@ -1,1 +1,28 @@
 #include "CircleList.h"
+
+
+string CircleList::Getpolinom(int Maxst)
+{
+	string str = "";
+	Monom* cur = first->GetNext();
+	while (cur != first)
+	{
+		string m = cur->GetMonom(Maxst);
+		if (m[0] == '-')
+		{
+			str = str + " " + m;
+
+		}
+		else
+		{
+			str = str + " + " + m;
+		}
+
+		cur = cur->GetNext();
+	}
+	if (str == "")
+	{
+		str = "0";
+	}
+	return str;
+}
